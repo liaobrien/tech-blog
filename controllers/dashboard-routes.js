e1f42b3750c939674c5bdfc77b264b53c762939d
Lia OBrien <liaobrien123@gmail.com>
2021-10-17 13:20:12 -0400

    created dashboard-routes.js for making and deleting posts, layed out outline for the routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { Post, Comment, User } = require('../models');
+
+// I think I will need the following requests:
+// GET the logged in user's posts
+
+
+// POST a new post
+
+
+// PUT (update) a user's post
+
+
+// DELETE a user's post
