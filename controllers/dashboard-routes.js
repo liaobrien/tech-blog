ce98b9a42a3641e4cc7f83c424340635563005f4
Lia OBrien <liaobrien123@gmail.com>
2021-10-18 20:51:30 -0400

    make first get req in dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -4,7 +4,23 @@ const withAuth = require('../utils/auth');
 
 // I think I will need the following requests:
 // GET the logged in user's posts
+router.get('/', withAuth, async (req, res) => {
+      try {
+            const dbPostData = await Post.findAll({
+                  where: {
+                        user_id: req.session.user_id
+                  }
+            });
 
+            const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+            res.render('dashboard', { posts });
+      } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+      }
+
+});
 
 // POST a new post
 
