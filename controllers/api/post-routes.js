f8546e1090fb7083c54e5cea23748ed0fafbaf25
Lia OBrien <liaobrien123@gmail.com>
2021-10-25 19:32:26 -0400

    created post routes file, update post now works on the back end

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const { Post, Comment, User } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// I need the following routes:
+
+// POST a new post
+// router.post('/')
+
+// PUT (update) a post
+router.put('/:id', async (req, res) => {
+      try {
+            const postData = await Post.update(req.body, {
+                  where: {
+                        id: req.params.id,
+                  }
+            });
+            if (!postData[0]) {
+                  res.status(404).json({ message: 'No post with this id!' });
+                  return;
+            }
+            res.status(200).json(postData);
+      } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+      }
+})
+
+// DELETE a post
+// router.delete('/:id')
+
+module.exports = router;
